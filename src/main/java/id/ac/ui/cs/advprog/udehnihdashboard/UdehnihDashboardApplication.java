f307d2238c5a19c9ad1e98420be6b03a89cb9b6d
Aditya <idpadityarahman@gmail.com>
2025-04-11 16:28:58 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/UdehnihDashboardApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdehnihDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdehnihDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihdashboard/UdehnihDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.udehnihdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdehnihDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
