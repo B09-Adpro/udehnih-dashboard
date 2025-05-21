25a5e0c7406de0d9554d024ac0b5f3b7dbe2ac2a
Aditya <idpadityarahman@gmail.com>
2025-05-21 10:54:31 +0700

    green: implement payment dto

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/dto/Payment.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.udehnihdashboard.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Payment {
+    private String transactionId;
+    private String userId;
+    private String userName;
+    private String courseId;
+    private String courseTitle;
+    private String tutorName;
+    private int amount;
+    private String status;
+    private String method;
+    private LocalDateTime createdAt;
+}
