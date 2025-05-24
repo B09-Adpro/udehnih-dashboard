560b3d104ba4078744fdf379624d3af1fd43884a
Aditya <idpadityarahman@gmail.com>
2025-05-24 13:47:10 +0700

    feat: implement get user info to auth feign

--- src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/feign/Auth.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/feign/Auth.java
@@ -1,8 +1,15 @@
 package id.ac.ui.cs.advprog.udehnihdashboard.feign;
 
+import id.ac.ui.cs.advprog.udehnihdashboard.dto.auth.AuthUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name="auth", url="https://udehnih-please-bisa-env.eba-uvfzaim3.us-east-1.elasticbeanstalk.com/")
 public interface Auth {
+
+    @RequestMapping(method = RequestMethod.GET, value="/api/users/{userId}")
+    AuthUserInfoResponse getUserInfo(@PathVariable("userId") String userId);
 
 }
