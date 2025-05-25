35b74c7a0546b9204e897506a42f7e6738ddf2e1
Aditya <idpadityarahman@gmail.com>
2025-05-25 17:21:50 +0700

    green: added tutor client get courses response dto

---
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/dto/tutor/TutorCoursesResponse.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.udehnihdashboard.dto.tutor;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TutorCoursesResponse {
+    List<TutorCourse> courses;
+}
