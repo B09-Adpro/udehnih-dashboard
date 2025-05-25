515be726bfa4e0da742d14c29ddc94c5bc340c4a
Aditya <idpadityarahman@gmail.com>
2025-05-25 16:13:14 +0700

    feat: added new api in auth client

--- src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/client/Auth.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/client/Auth.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.udehnihdashboard.client;
 
 import id.ac.ui.cs.advprog.udehnihdashboard.config.FeignConfig;
+import id.ac.ui.cs.advprog.udehnihdashboard.dto.auth.AuthAddRoleRequest;
+import id.ac.ui.cs.advprog.udehnihdashboard.dto.auth.AuthAddRoleResponse;
 import id.ac.ui.cs.advprog.udehnihdashboard.dto.auth.AuthUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,4 +17,6 @@ public interface Auth {
     @RequestMapping(method = RequestMethod.GET, value="/api/users/{userId}")
     AuthUserInfoResponse getUserInfo(@PathVariable("userId") String userId);
 
+    @RequestMapping(method = RequestMethod.POST, value="/api/roles/add")
+    AuthAddRoleResponse addRole(AuthAddRoleRequest request);
 }
