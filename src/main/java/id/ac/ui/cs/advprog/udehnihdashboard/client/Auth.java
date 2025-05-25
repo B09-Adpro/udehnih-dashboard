03405e654b4fc7ead0a61be7f876ed2004fa7f2b
Aditya <idpadityarahman@gmail.com>
2025-05-25 15:34:37 +0700

    red: add auth client add role request test

--- src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/client/Auth.java
+++ src/main/java/id/ac/ui/cs/advprog/udehnihdashboard/client/Auth.java
@@ -1,12 +1,15 @@
 package id.ac.ui.cs.advprog.udehnihdashboard.client;
 
+import id.ac.ui.cs.advprog.udehnihdashboard.config.FeignConfig;
 import id.ac.ui.cs.advprog.udehnihdashboard.dto.auth.AuthUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="auth", url="auth-temp.com")
+@FeignClient(
+        name="auth", url="auth-temp.com",
+        configuration = FeignConfig.class)
 public interface Auth {
 
     @RequestMapping(method = RequestMethod.GET, value="/api/users/{userId}")
---
+++ src/test/java/id/ac/ui/cs/advprog/udehnihdashboard/dto/auth/AuthAddRoleRequestTest.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.udehnihdashboard.dto.auth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AuthAddRoleRequestTest {
+    private AuthAddRoleRequest request;
+
+    @BeforeEach
+    public void setUp() {
+        this.request = new AuthAddRoleRequest;
+        this.request.setId(123L);
+        this.request.setRoleType(RoleType.STUDENT);
+    }
+
+    @Test
+    public void testGetAttributes() {
+        assertEquals(123L, request.getId());
+        assertEquals(RoleType.STUDENT, request.getRoleType());
+    }
+}
